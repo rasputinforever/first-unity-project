3a47419841b5438f2984605599fe5d9c7a1e55bb
Erik Portillo <erik.justin.portillo@gmail.com>
2024-01-10 20:19:58 -0800

    functional jumping and platforms

--- First Game Test/Assets/controls.cs
+++ First Game Test/Assets/controls.cs
@@ -1,23 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class controls : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D myRigidbody;
-    // Start is called before the first frame update
-    void Start()
-    {
-         gameObject.name = "test";
-    }
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
 
-    // Update is called once per frame
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true) 
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded())
         {
-            myRigidbody.velocity = Vector2.up * 10;
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
-        
+
+        if (Input.GetKeyUp(KeyCode.UpArrow) && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
     }
-} 
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
